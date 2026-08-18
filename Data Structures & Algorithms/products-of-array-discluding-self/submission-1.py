ac2a4d2bd57a82aacc6d536687816bc676ac98e6
Vladislav Zolotukhin <144418975+vladdyz@users.noreply.github.com>
2026-08-18 17:35:05 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = []
+        product = 1
+        zeroFlag = 0 # this used to be a bool but the actual non-example test cases can contain more than one zero
+        for i in range(len(nums)):
+            # handle zero cases in the next loop
+            if nums[i] != 0:
+                product *= nums[i]
+            else:
+                zeroFlag += 1
+                
+        for i in range(len(nums)):
+            if zeroFlag > 1:
+                output.append(0)
+            else: 
+                if zeroFlag and nums[i] != 0:
+                    output.append(0)
+                else: 
+                    output.append(product)
+        for i in range(len(output)):
+            if nums[i] != 0:
+                output[i] /= nums[i]
+            # else:
+            #     output[i] 
+            # just leave it alone
+            output[i] = int(output[i])
+
+
+        return output
+
+        
